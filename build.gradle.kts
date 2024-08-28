bffb8d7a002ca99df2c041f2e9d3576054b174a2
seungdols <seungdols0822@gmail.com>
2024-08-28 22:40:11 +0900

    feat: Modify build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,111 @@
+import kotlinx.kover.gradle.plugin.dsl.AggregationType
+import kotlinx.kover.gradle.plugin.dsl.CoverageUnit
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+//tasks.bootJar { enabled = false }
+//tasks.jar { enabled = true }
+
+val rootProject = project
+
 plugins {
-    kotlin("jvm") version "2.0.0"
+    id("org.springframework.boot") version Versions.springBootVersion
+    id("io.spring.dependency-management") version Versions.springDependencyManagementVersion
+    id("org.jlleitschuh.gradle.ktlint") version Versions.ktlintVersion
+    id("org.jetbrains.kotlin.plugin.noarg") version Versions.kotlinVersion
+    id("org.jetbrains.kotlinx.kover") version Versions.koverVersion
+    id("com.adarshr.test-logger") version Versions.adarshrTestLoggerVersion
+    kotlin("kapt") version Versions.kotlinVersion
+    kotlin("jvm") version Versions.kotlinVersion
+    kotlin("plugin.spring") version Versions.kotlinVersion
 }
 
-group = "com.seungdols.company"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "com.seungdols.company"
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = "https://plugins.gradle.org/m2/")
+        maven(url = "https://repo.spring.io/snapshot")
+        maven(url = "https://repo.spring.io/milestone")
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
+subprojects {
+    apply {
+        plugin("kotlin")
+        plugin("kotlin-kapt")
+        plugin("kotlin-spring")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("org.jlleitschuh.gradle.ktlint")
+        plugin("org.jetbrains.kotlin.plugin.noarg")
+
+        plugin("org.jetbrains.kotlinx.kover")
+        plugin("com.adarshr.test-logger")
+        plugin("idea")
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = JavaVersion.VERSION_17.majorVersion
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
+        forkEvery = 100
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    dependencies {
+        implementation(Dependencies.KOTLIN)
+        implementation(Dependencies.JACKSON)
+        implementation(Dependencies.LOGGING)
+        kapt(Dependencies.SPRING_BOOT_CONFIGURATION_PROCESSOR)
+        testImplementation(Dependencies.TEST)
+        // Add modules for kover
+        kover(project(path))
+    }
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        filter {
+            exclude { it.file.path.contains("generated") }
+        }
+    }
 }
 
-tasks.test {
-    useJUnitPlatform()
+kover {
+    useJacoco()
+    // https://github.com/Kotlin/kotlinx-kover/blob/v0.8.0/docs/gradle-plugin/migrations/migration-to-0.8.0.md
+    reports {
+        total {
+            xml {
+                onCheck = true
+                file(layout.buildDirectory.file("reports/kover/report.xml"))
+            }
+            verify {
+                onCheck = false
+                rule {
+                    groupBy = kotlinx.kover.gradle.plugin.dsl.GroupingEntityType.APPLICATION
+
+                    bound {
+                        minValue = 80
+                        coverageUnits = CoverageUnit.LINE
+                        aggregationForGroup = AggregationType.COVERED_PERCENTAGE
+                    }
+                }
+            }
+        }
+    }
 }
-kotlin {
-    jvmToolchain(17)
+
+testlogger {
+    setTheme("mocha")
 }
